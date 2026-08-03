1fc76eb11dd432482d0995b1dc6951e2f4b7ef53
uitcis <uitcis@163.com>
2026-08-03 17:44:44 +0800

    清理无用文件: 删除构建日志、vssver.scc、SDK安装脚本、反编译临时目录

--- accept_license.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Microsoft\jdk-17.0.20.8-hotspot
-set ANDROID_HOME=%LOCALAPPDATA%\Android\Sdk
-set ANDROID_SDK_ROOT=%LOCALAPPDATA%\Android\Sdk
-echo y | "%LOCALAPPDATA%\Android\Sdk\cmdline-tools\latest\bin\sdkmanager.bat" --sdk_root="%LOCALAPPDATA%\Android\Sdk" --licenses
